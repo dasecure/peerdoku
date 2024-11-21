69ca00ef7581f4f9d90aeb30b09a05ba8ecd0413
dasecure <45675441+dasecure@users.noreply.github.com>
2024-11-20 20:45:46 -0800

    Initial Commit

---
+++ peerdokuTests/peerdokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  peerdokuTests.swift
+//  peerdokuTests
+//
+//  Created by Vincent Ooi on 11/20/24.
+//
+
+import Testing
+@testable import peerdoku
+
+struct peerdokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
